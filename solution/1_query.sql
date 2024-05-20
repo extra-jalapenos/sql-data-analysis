e7d587006b5c4e05e09a0e6f2ffebdf4c9eda8be
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-05-20 11:55:21 +0200

    queries to check connection and upload of tables

---
+++ solution/1_query.sql
@@ -0,0 +1,4 @@
+SELECT * FROM `sql-sandbox-20240505.final_exercise.leagues` LIMIT 10;
+SELECT * FROM `sql-sandbox-20240505.final_exercise.match` LIMIT 10;
+SELECT * FROM `sql-sandbox-20240505.final_exercise.team` LIMIT 10;
+SELECT * FROM `sql-sandbox-20240505.final_exercise.player` LIMIT 10;
