3b70f336c52ab949a0788fc4abae2a0a6c8d6600
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-05-26 18:50:03 +0200

    i choose to interpret mid-range as NOT median but mid-point between min and max.

--- solution/4_query.sql
+++ solution/4_query.sql
@@ -2,6 +2,7 @@ SELECT
   name, season,
   MIN(home_team_goal) AS min_home_goal,
   AVG(home_team_goal) AS avg_home_goal,
+  (MAX(home_team_goal) - MIN(home_team_goal)) / 2 + MIN(home_team_goal) as mid_range,
   MAX(home_team_goal) AS max_home_goal,
   SUM(home_team_goal) AS sum_home_goal,
 	FROM `sql-sandbox-20240505.final_exercise.match` AS match
@@ -10,5 +11,5 @@ SELECT
   GROUP BY name, season
 	ORDER BY sum_home_goal DESC
 
--- this does not yet include the mid range i know
+-- i choose to interpret mid-range as NOT median but mid-point between min and max.
 -- ENGLAND PREMIER LEAGUE of 2009/2010 had the most home goals.
